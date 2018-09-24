be6d33ac154064ff3fd3d520201644ad44286d62
Danielle Meerburg <danielle_meerburg@live.nl>
2018-09-24 16:30:10 +0200

    fixed migration bug

---
+++ app/Http/Controllers/ArtController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\art;
+use Illuminate\Http\Request;
+
+class ArtController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\art  $art
+     * @return \Illuminate\Http\Response
+     */
+    public function show(art $art)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\art  $art
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(art $art)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\art  $art
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, art $art)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\art  $art
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(art $art)
+    {
+        //
+    }
+}
