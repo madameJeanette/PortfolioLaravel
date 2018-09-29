22a3f6f2e611be185e398c22aa67625a54ce96c6
Danielle Meerburg <danielle_meerburg@live.nl>
2018-09-29 19:22:53 +0200

    changed spacing and letter size create pg.

--- resources/views/art/create.blade.php
+++ resources/views/art/create.blade.php
@@ -3,11 +3,13 @@
 @section('content')
 <br>
 <h1> Upload nieuwe werken. </h1>
-
+<hr>
 <form>
     <div class="form-group">
-        <label for="exampleInputFile">Naam</label>
-        <input type="text" class="form-control" placeholder="Naam">
+        <label for="exampleInputFile"><h4>Naam werk</h4></label>
+        <input type="text" class="form-control" placeholder="Schattige panda.">
+    </div>
+    
     <div class="form-group">
       <label for="exampleInputFile">Upload werk</label>
       <input type="file" id="exampleInputFile">
