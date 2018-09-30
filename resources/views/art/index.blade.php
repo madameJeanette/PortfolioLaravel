2c7f2977aff2472de006b0f791f8be960d27cafd
Danielle Meerburg <danielle_meerburg@live.nl>
2018-09-30 20:04:47 +0200

    cngd btn name upload.

--- resources/views/art/index.blade.php
+++ resources/views/art/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <h1> Recente werken. </h1>
-<a class="btn btn-primary" href="/arts/create" role="button">Upload</a>
+
 @if(count($arts)>0)
  @foreach($arts as $art)
   <div class = "well">
@@ -10,9 +10,10 @@
   <img src="\img\{{$art->picture }}" /><br>
   <small> uploaded:{{$art->created_at}}</small>
  </div>
-
  @endforeach
  {{$arts->links()}}
+
+ <a class="btn btn-primary" href="/arts/create" role="button">Upload</a>
 @else
 <p> Geen afbeeldingen gevonden....</p>
 @endif
