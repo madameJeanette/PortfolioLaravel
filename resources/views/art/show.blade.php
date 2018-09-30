9b8b8e9cbeb4fa0d82db090bdd85bbe55a818d0e
Danielle Meerburg <danielle_meerburg@live.nl>
2018-09-30 17:38:22 +0200

    added go back button show.blade

--- resources/views/art/show.blade.php
+++ resources/views/art/show.blade.php
@@ -1,6 +1,12 @@
 @extends('layout.app')
 
 @section('content')
-<h1> {{$art->name}} </h1>
+<a class="btn btn-primary" href="/arts" role="button">Ga terug</a>
 
+<h1> {{$art->name}} </h1>
+ <div>
+    <img src="\img\{{$art->picture }}" /><br>
+ </div>
+ <hr>
+ <small> uploaded:{{$art->created_at}}</small>
 @endsection
