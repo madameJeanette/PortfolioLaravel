37b42c97947a7f690881a1cc1bc7c2c525ce78f5
Danielle Meerburg <danielle_meerburg@live.nl>
2018-10-22 21:15:42 +0200

    fixed redirect bug crud

--- app/Http/Controllers/ArtsController.php
+++ app/Http/Controllers/ArtsController.php
@@ -16,7 +16,7 @@ class ArtsController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth',['except'=>['index','show']]);
+        $this->middleware('auth:admin',['except'=>['index','show']]);
     }
     /**
      * Display a listing of the resource.
